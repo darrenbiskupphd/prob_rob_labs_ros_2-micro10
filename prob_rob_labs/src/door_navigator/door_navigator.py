76d42bb929deaf582bb91816c9a573c15ceb02bb
darrenbiskupphd <db3728@columbia.edu>
2025-10-02 18:47:20 -0400

    complete lab3 assignment 4

--- prob_rob_labs/src/door_navigator/door_navigator.py
+++ prob_rob_labs/src/door_navigator/door_navigator.py
@@ -12,24 +12,25 @@ class OpenDoorThroughDoor(Node):
     def __init__(self):
         super().__init__('open_door_through_door')
         self.log = self.get_logger()
-        self.declare_parameter('time_open', 6.0)
-        self.declare_parameter('time_close', 11.0)
+        self.feature_mean_sub = self.create_subscription(Float64, '/feature_mean',self.feature_mean_callback,10)
+        self.latest_feature_mean = 300
 
-        
-        self.declare_parameter('speed_forward', 1.2)
-        self.declare_parameter('speed_stop', 0.0)
-        
-        self.declare_parameter('torque_open', 2.0)
-        self.declare_parameter('torque_close', -1.8)
+        self.declare_parameter("speed_forward", 5.0)
+        self.declare_parameter("speed_stop", 0.0)
+        self.declare_parameter("torque_open", 2.0)
+        self.declare_parameter("torque_close", -2.0)
 
-        
         self.pub_door_torque = self.create_publisher(Float64,'/hinged_glass_door/torque', 10)
         self.pub_robot_vel = self.create_publisher(Twist,'/cmd_vel', 10)
 
         self.timer = self.create_timer(heartbeat_period, self.heartbeat)
         
+        self.move_started = 0
+        self.state = 0
         self.t = 0.0
 
+    def feature_mean_callback(self, msg):
+        self.latest_feature_mean = float(msg.data)
         
     def send_torque(self,torque):
         self.pub_door_torque.publish(Float64(data=torque))
@@ -48,21 +49,18 @@ class OpenDoorThroughDoor(Node):
     def heartbeat(self):
         # self.log.info('heartbeat')
         self.t += heartbeat_period
-        if self.t < self.get_parameter('time_open').value:
-            torque_open = self.get_parameter('torque_open').value
-            self.send_torque(torque_open)
+        if self.state == 0:
+            self.send_torque(self.get_parameter("torque_open").value)
+            if self.latest_feature_mean < 260.0:
+                self.state = 1
+                self.move_started = self.t
+        elif self.state == 1:
+            self.robot_forward(self.get_parameter("speed_forward").value)
+            if self.t - self.move_started > 8.0:
+                self.state = 2
+        elif self.state == 2:
             self.robot_stop()
-
-        elif self.t < self.get_parameter('time_close').value:
-            speed_forward = self.get_parameter('speed_forward').value
-            self.robot_forward(speed_forward)
-
-        else:
-            torque_close = self.get_parameter('torque_close').value
-            self.send_torque(torque_close)
-            self.robot_stop()
-            
-
+            self.send_torque(self.get_parameter("torque_close").value)
 
     def spin(self):
         rclpy.spin(self)
