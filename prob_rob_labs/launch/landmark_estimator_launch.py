67a643112449e790d78fa81611898820188f05d2
darrenbiskupphd <db3728@columbia.edu>
2025-11-06 19:54:36 -0500

    grade lab5 assignment 2

--- prob_rob_labs/launch/landmark_estimator_launch.py
+++ prob_rob_labs/launch/landmark_estimator_launch.py
@@ -10,10 +10,19 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='true',
                               description='set to true for simulation'),
+        DeclareLaunchArgument('cylinder_height', default_value='0.5',
+                              description='height of cylinder landmark'),
+        DeclareLaunchArgument('cylinder_radius', default_value='0.1',
+                              description='radius of cylinder landmark'),
+        DeclareLaunchArgument('cylinder_color', default_value='cyan',
+                              description='color of cylinder landmark'),
         Node(
             package='prob_rob_labs',
             executable='landmark_estimator',
             name='landmark_estimator',
-            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time'),
+                         'cylinder_height': LaunchConfiguration('cylinder_height'),
+                         'cylinder_radius': LaunchConfiguration('cylinder_radius'),
+                         'cylinder_color': LaunchConfiguration('cylinder_color')}]
         )
     ])
--- prob_rob_labs/src/landmark_estimator/landmark_estimator.py
+++ prob_rob_labs/src/landmark_estimator/landmark_estimator.py
@@ -1,7 +1,10 @@
+import numpy as np
 import rclpy
 from rclpy.node import Node
 
 from prob_rob_msgs.msg import Point2DArrayStamped
+from sensor_msgs.msg import CameraInfo
+from std_msgs.msg import Float32MultiArray
 
 heartbeat_period = 0.1
 
@@ -11,29 +14,58 @@ class LandmarkEstimator(Node):
         super().__init__('landmark_estimator')
         self.log = self.get_logger()
         # self.timer = self.create_timer(heartbeat_period, self.heartbeat)
-
-        color = 'cyan'
+        
+        self.landmark_pixel_axis = -1
+        self.landmark_height_pixels = -1
+        
+        self.declare_parameter('cylinder_height', 0.5)
+        self.declare_parameter('cylinder_radius', 0.1)
+        self.declare_parameter('cylinder_color', "cyan")
+        color = self.get_parameter('cylinder_color').value
         topic_name = f'/vision_{color}/corners'
 
+        self.pub_bearing_flag = False
+
         self.corner_sub = self.create_subscription(Point2DArrayStamped, topic_name, self.estimate_landmark_height_pos, 10) #time stamp for every 0.006s
+        self.camera_sub = self.create_subscription(CameraInfo, '/camera/camera_info', self.estimate_dist_bearing, 10) #time stamp for every 0.006s
+        self.bearing_dist_pub = self.create_publisher(Float32MultiArray, 'bearing_dist', 10)
+
 
     def estimate_landmark_height_pos(self, msg):
-        min_x = 100000000.0
-        min_y = 100000000.0
-        max_x = 0.0
-        max_y = 0.0
-        for point in msg.points:
-            min_x = min(point.x, min_x)
-            min_y = min(point.y, min_y)
-            max_x = max(point.x, max_x)
-            max_y = max(point.y, max_y)
+        if len(msg.points) < 1:
+            self.pub_bearing_flag = False
+        else:
+            self.pub_bearing_flag = True
+            min_x = 100000000.0
+            min_y = 100000000.0
+            max_x = 0.0
+            max_y = 0.0
+            for point in msg.points:
+                min_x = min(point.x, min_x)
+                min_y = min(point.y, min_y)
+                max_x = max(point.x, max_x)
+                max_y = max(point.y, max_y)
 
-        # self.log.info(f"Center X: {(max_x + min_x)/2}, Height: {(max_y - min_y)}")
+            # self.log.info(f"Center X: {(max_x + min_x)/2}, Height: {(max_y - min_y)}")
+            self.landmark_pixel_axis = (max_x + min_x)/2
+            self.landmark_height_pixels = max_y - min_y
 
-        return (max_x + min_x)/2 , (max_y - min_y)
+    def estimate_dist_bearing(self, msg):
+        if self.pub_bearing_flag:
+            p = msg.k
+            fx = p[0]
+            cx = p[2]
+            fy = p[4]
+            cy = p[5]
+            theta = np.arctan2(cx - self.landmark_pixel_axis, fx)
+            h = self.get_parameter('cylinder_height').value
+            d = h*(fy/self.landmark_height_pixels*np.cos(theta))
+
+            msg = Float32MultiArray()
+            msg.data = [theta, d]
+            self.bearing_dist_pub.publish(msg)
 
 
-        
     def heartbeat(self):
         self.log.info('heartbeat')
 
